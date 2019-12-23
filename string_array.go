0059a3c3e1fa4dd68fb942dc54efd6d2a27b4435
Laurent Sauvage <sauvage.laurent@gmail.com>
2019-12-23 16:26:39 +0100

    Add getter to string_array structure

--- string_array.go
+++ string_array.go
@@ -11,6 +11,14 @@ func (a *StringArray) Set(s string) error {
 	return nil
 }
 
+func (a *StringArray) Get() interface{} {
+    var s string
+    for _, v := range *a {
+        s += v
+    }
+    return s
+}
+
 func (a *StringArray) String() string {
 	return strings.Join(*a, ",")
 }
